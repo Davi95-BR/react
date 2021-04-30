42fd25a27f378a949f65141f0a87382e6934b12c
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-04-29 23:21:23 -0300

    Importando img svg e usando ReactComponent

--- src/componentes/CardNota/CardNota.jsx
+++ src/componentes/CardNota/CardNota.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './estilo.css'
-
+import {ReactComponent as DeleteSVG} from '../../assets/img/delete.svg'
 class CardNota extends Component {
     render() {
         return (
             <section className="card-nota">
-                <header className="card-nota__cabecalho">
+                <DeleteSVG />
+                <header className="card-nota__cabecalho" >
                     <h3 className="card-nota__titulo">{this.props.titulo}</h3>
                 </header>
                 <p className="card-nota__texto">{this.props.texto}</p>
