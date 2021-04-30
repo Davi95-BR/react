f935ce2b556324b6537e43cd5bbede570fa62f38
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-04-29 23:11:44 -0300

    Revert "Importando svg e utlizando o React e criando uma pasta de img"
    This reverts commit a6219b8e46b801c9f6b3adb331a4e7d31f9eb078.

--- src/componentes/CardNota/CardNota.jsx
+++ src/componentes/CardNota/CardNota.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './estilo.css'
-import {ReactComponent as DeleteSVG} from '../../assets/img/delete.svg' // O nome deleteSVG deve ser maísculo para ser reconhecido == DeleteSVG
 
 class CardNota extends Component {
     render() {
         return (
             <section className="card-nota">
-                <DeleteSVG/>
                 <header className="card-nota__cabecalho">
                     <h3 className="card-nota__titulo">{this.props.titulo}</h3>
                 </header>
