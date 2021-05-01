49923511492565e2dc3894e05446b63ca2562276
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-05-01 16:03:39 -0300

    Inserindo função de apagar o card

--- src/componentes/CardNota/CardNota.jsx
+++ src/componentes/CardNota/CardNota.jsx
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import './estilo.css'
 import {ReactComponent as DeleteSVG} from '../../assets/img/delete.svg'
 class CardNota extends Component {
+
+    apagar(){
+        const indice = this.props.indice;
+        this.props.apagarNota(indice)
+    }
+
     render() {
         return (
             <section className="card-nota">
-                <DeleteSVG />
                 <header className="card-nota__cabecalho" >
                     <h3 className="card-nota__titulo">{this.props.titulo}</h3>
                 </header>
-                <p className="card-nota__texto">{this.props.texto}</p>
+                <p className="card-nota__texto">{this.props.texto}</p><br></br>
+                <DeleteSVG onClick={this.apagar.bind(this)}/>
             </section>
         );
     }
