f62de4bfd74ab67644a73981c241108980280156
DaviMeneghelloDomingues <davi_meneghello1@hotmail.com>
2021-05-01 22:55:15 -0300

    Estilizando conteudo-principal e adicionando o evento onKeyUp no input para adicionar categoria

--- src/componentes/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/componentes/ListaDeCategorias/ListaDeCategorias.jsx
@@ -2,18 +2,27 @@ import React, { Component } from "react";
 import "./estilo.css"
 
 class ListaDeCategorias extends Component {
-    state = {}
+
+    _handlerEventoInput(e) {
+        if(e.key == "Enter"){
+            console.log("adiconar categoria")
+        }
+    }
 
     render() {
         return (
-            <section>
-                <ul>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
+            <section className="lista-categorias">
+                <input
+                    className="lista-categorias__input" type="text"
+                    placeholder="Adicionar categoria"
+                    onKeyUp={this._handlerEventoInput.bind(this)}
+                />
+                <ul className="lista-categorias__lista">
+                    <li className="lista-categorias__item" >Categorias</li>
+                    <li className="lista-categorias__item">Categorias</li>
+                    <li className="lista-categorias__item">Categorias</li>
+                    <li className="lista-categorias__item">Categorias</li>
                 </ul>
-                <input type="text" />
             </section>
         );
     }
